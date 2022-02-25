0a9620367b11b5fd2b272a9df3349f989f1c80d8
Eric Toreki <etoreki@udel.edu>
2022-02-24 23:46:13 -0500

    toCSV passing tests

--- src/nested.ts
+++ src/nested.ts
@@ -104,7 +104,16 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const header = "id,name,options,points,published\n";
+    return (
+        header +
+        questions
+            .map(
+                (question: Question): string =>
+                    `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+            )
+            .join("\n")
+    );
 }
 
 /**
