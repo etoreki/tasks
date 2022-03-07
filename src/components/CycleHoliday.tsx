43bb03cc6b97a0d996f1625396b0c4abce844ee0
Eric Toreki <etoreki@udel.edu>
2022-03-07 15:39:21 -0500

    CycleHodiay passing tests

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,42 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+type Holiday = "🎄" | "🦃" | "🎃" | "❤️" | "🐰";
+
+const NEXT_ALPHABETICAL_HOLIDAY: Record<Holiday, Holiday> = {
+    "🎄": "🐰",
+    "🐰": "🎃",
+    "🎃": "🦃",
+    "🦃": "❤️",
+    "❤️": "🎄"
+};
+
+const NEXT_YEARLY_HOLIDAY: Record<Holiday, Holiday> = {
+    "🎄": "❤️",
+    "🐰": "🎃",
+    "🎃": "🦃",
+    "🦃": "🎄",
+    "❤️": "🐰"
+};
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🎃");
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <div>
+                <Button
+                    onClick={() =>
+                        setHoliday(NEXT_ALPHABETICAL_HOLIDAY[holiday])
+                    }
+                >
+                    Advance by Alphabet
+                </Button>
+                <Button
+                    onClick={() => setHoliday(NEXT_YEARLY_HOLIDAY[holiday])}
+                >
+                    Advance by Year
+                </Button>
+            </div>
+        </div>
+    );
 }
