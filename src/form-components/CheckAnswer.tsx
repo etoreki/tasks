d8abdcaab1cc7c413e02826b3197bde2248fd0fb
Eric Toreki <etoreki@udel.edu>
2022-03-09 23:56:03 -0500

    CheckAnswer passing tests and functioning locally

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    function updateAnswer(event: ChangeEvent) {
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formMovieName">
+                <Form.Label>Your Answer:</Form.Label>
+                <Form.Control value={userAnswer} onChange={updateAnswer} />
+            </Form.Group>
+            <span>{userAnswer === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
