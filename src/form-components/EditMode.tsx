b3e627af8a0bdb7d8d84ed142c4b5224db51c17d
Eric Toreki <etoreki@udel.edu>
2022-03-13 16:02:46 -0400

    EditMode working locally and passing tests

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <div>
+                <Form.Check
+                    type="switch"
+                    id="is-edit-mode"
+                    label="Editing"
+                    checked={isEditing}
+                    onChange={() => setIsEditing(!isEditing)}
+                />
+            </div>
+            <div>
+                {isEditing ? (
+                    <>
+                        <Form.Group controlId="formStudentName">
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control value={name} onChange={updateName} />
+                        </Form.Group>
+                        <Form.Check
+                            type="checkbox"
+                            id="is-student-check"
+                            label="Is a student?"
+                            checked={isStudent}
+                            onChange={() => setIsStudent(!isStudent)}
+                        />
+                    </>
+                ) : (
+                    <span>
+                        {name} is {isStudent ? "" : "not"} a student.
+                    </span>
+                )}
+            </div>
         </div>
     );
 }
