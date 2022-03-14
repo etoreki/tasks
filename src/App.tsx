c22ba82b9130378d8864a81c213ce0284af6ac74
etoreki <79226120+etoreki@users.noreply.github.com>
2022-03-14 10:38:41 -0400

    Update App.tsx
    Added missing >

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App(): JSX.Element {
             />
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
-            </Button
+            </Button>
             <Container>
                 <Row>
                     <Col>
