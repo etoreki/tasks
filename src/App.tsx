f46449cf4c4ac9732daa171fa53c3fd483a94e97
Eric Toreki <etoreki@udel.edu>
2022-03-14 23:50:55 -0400

    Added Show/Hide button for old tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -16,47 +16,65 @@ import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
+import { Button } from "react-bootstrap";
+
+export function ShowHideTasks(): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(false);
+    return (
+        <div>
+            {visible && (
+                <div>
+                    <header className="App-header">
+                        UD CISC275 with React Hooks and TypeScript and Eric
+                        Toreki
+                    </header>
+                    <hr></hr>
+                    <CheckAnswer expectedAnswer="42"></CheckAnswer>
+                    <hr></hr>
+                    <GiveAttempts></GiveAttempts>
+                    <hr></hr>
+                    <EditMode></EditMode>
+                    <hr></hr>
+                    <ChangeColor></ChangeColor>
+                    <hr></hr>
+                    <MultipleChoiceQuestion
+                        options={["a", "b", "c"]}
+                        expectedAnswer="b"
+                    ></MultipleChoiceQuestion>
+                    <hr></hr>
+                    {<DoubleHalf></DoubleHalf>}
+                    <hr></hr>
+                    <ChooseTeam></ChooseTeam>
+                    <hr></hr>
+                    <ColoredBox></ColoredBox>
+                    <hr></hr>
+                    <ShoveBox></ShoveBox>
+                    <hr></hr>
+                    <Counter></Counter>
+                    <hr />
+                    <RevealAnswer></RevealAnswer>
+                    <hr />
+                    <StartAttempt></StartAttempt>
+                    <hr />
+                    <TwoDice></TwoDice>
+                    <hr />
+                    <ChangeType></ChangeType>
+                    <hr />
+                    <CycleHoliday></CycleHoliday>
+                </div>
+            )}
+            <Button onClick={() => setVisible(!visible)}>
+                Show/Hide Old Tasks
+            </Button>
+        </div>
+    );
+}
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript and Eric Toreki
-            </header>
             <Quizzer></Quizzer>
-            <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
-            <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            {<DoubleHalf></DoubleHalf>}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            <ShowHideTasks></ShowHideTasks>
         </div>
     );
 }
