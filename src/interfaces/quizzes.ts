88b60934ffac30d9012ff725b9cf5facd6a00fbc
Eric Toreki <etoreki@udel.edu>
2022-03-17 15:13:17 -0400

    First attempt at displaying quizzes, need data

--- src/interfaces/quizzes.ts
+++
@@ -1,7 +0,0 @@
-import { Quiz } from "./quiz";
-
-/** A representation of a list of Quizzed in a quizzing application */
-export interface Quizzes {
-    /** The list of all quizzes */
-    quizzes: Quiz[];
-}
--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Quiz } from "../interfaces/quiz";
 
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    const [quizzes, setQuizzes] = useState<Quiz[]>();
+    return (
+        <div>
+            <h3>Quizzer</h3>
+            {quizzes.map((quiz: Quiz) => (
+                <Row key={quiz.name}>
+                    <Col>{quiz.name}</Col>
+                    <Col>{quiz.description}</Col>
+                    <Col>There are {quiz.questions.length} questions.</Col>
+                </Row>
+            ))}
+        </div>
+    );
 }
