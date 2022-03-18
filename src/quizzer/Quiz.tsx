3d2617d099b73697c3f183f08f634b8661859519
Eric Toreki <etoreki@udel.edu>
2022-03-18 16:18:12 -0400

    Added checkbox for published questions on Quiz

--- src/quizzer/Quiz.tsx
+++ src/quizzer/Quiz.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function Quiz(): JSX.Element {
-    return <h3>Quiz</h3>;
+    const [onlyPublished, setOnlyPublished] = useState<boolean>(false);
+    return (
+        <div>
+            <div>
+                <h3>Quiz</h3>
+                <Form.Check
+                    type="checkbox"
+                    id="show-published-check"
+                    label="Only Show Published Questions"
+                    checked={onlyPublished}
+                    onChange={() => setOnlyPublished(!onlyPublished)}
+                />
+            </div>
+        </div>
+    );
 }
