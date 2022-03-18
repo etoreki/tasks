627fc8bfa5effcee313fcba2757942e9e821bb39
Eric Toreki <etoreki@udel.edu>
2022-03-17 22:08:35 -0400

    Can add and delete quizzes need testing

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { Quiz } from "../interfaces/quiz";
 import sampleQuizzes from "../data/sample_quizzes.json";
-import { Question, QuestionType } from "../interfaces/question";
+import { Question } from "../interfaces/question";
 
 const QUIZZES = sampleQuizzes.map(
     (quiz): Quiz => ({
@@ -22,17 +22,54 @@ const QUIZZES = sampleQuizzes.map(
 export function Quizzer(): JSX.Element {
     const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
     const [currentQuiz, setCurrentQuiz] = useState<Quiz>(quizzes[0]);
+    const [addingQuiz, setAddingQuiz] = useState<boolean>(false);
+    const [newQuizName, setNewQuizName] = useState<string>("");
+    const [newQuizDesc, setNewQuizDesc] = useState<string>("");
     function setCurrentQuizByName(expectedName: string) {
         const newQuiz = quizzes.findIndex(
             (quiz: Quiz): boolean => quiz.name === expectedName
         );
         setCurrentQuiz(quizzes[newQuiz]);
     }
+    function removeQuiz() {
+        setQuizzes(
+            quizzes.filter(
+                (quiz: Quiz): boolean => quiz.name !== currentQuiz.name
+            )
+        );
+        setCurrentQuiz(quizzes[0]);
+    }
+    function addQuiz() {
+        setQuizzes([
+            ...quizzes,
+            { name: newQuizName, description: newQuizDesc, questions: [] }
+        ]);
+        setAddingQuiz(false);
+        setNewQuizDesc("");
+        setNewQuizName("");
+    }
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setNewQuizName(event.target.value);
+    }
+    function updateDesc(event: React.ChangeEvent<HTMLInputElement>) {
+        setNewQuizDesc(event.target.value);
+    }
     return (
         <div>
             <h3>Quizzer</h3>
+            <Row style={{ border: "1px solid black" }}>
+                <Col>
+                    <strong>Quiz Name</strong>
+                </Col>
+                <Col>
+                    <strong>Description</strong>
+                </Col>
+                <Col>
+                    <strong>Number of Questions</strong>
+                </Col>
+            </Row>
             {quizzes.map((quiz: Quiz) => (
-                <Row key={quiz.name}>
+                <Row style={{ border: "1px solid black" }} key={quiz.name}>
                     <Col>
                         {" "}
                         <Form.Check
@@ -53,6 +90,33 @@ export function Quizzer(): JSX.Element {
                     <Col>There are {quiz.questions.length} questions.</Col>
                 </Row>
             ))}
+            <div>
+                <Button onClick={() => setAddingQuiz(!addingQuiz)}>
+                    Add Quiz
+                </Button>
+                <Button onClick={removeQuiz}>Delete Seleccted Quiz</Button>
+            </div>
+            <div>
+                {addingQuiz && (
+                    <div>
+                        <Form.Group controlId="formQuizName">
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control
+                                value={newQuizName}
+                                onChange={updateName}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formQuizDescription">
+                            <Form.Label>Quiz Description:</Form.Label>
+                            <Form.Control
+                                value={newQuizDesc}
+                                onChange={updateDesc}
+                            />
+                        </Form.Group>
+                        <Button onClick={addQuiz}>Add Quiz</Button>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
